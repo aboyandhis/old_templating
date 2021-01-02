f0ed89e1ee97c2ebd28f0fc97d27478deb81c577
Patrrck Kelly <hey@butterflydesin.me>
2021-01-01 23:15:36 -0700

    Darkmode. Defaults to system font. Provider managing theme state.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_template/providers/auth.dart';
 import 'package:firebase_template/providers/data.dart';
 import 'package:firebase_template/providers/state.dart';
+import 'package:firebase_template/providers/theme.dart';
 import 'package:firebase_template/screens/home.dart';
 
 import 'package:flutter/material.dart';
@@ -16,62 +17,29 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var primary = Colors.purple;
-    var primaryColor = Colors.purple[200];
-    var accentColor = Colors.purple[200];
-    var darkSurfaceColor = Colors.black87;
-    // ignore: unused_local_variable
-    var lightSurfaceColor = Colors.grey[100];
-    // ignore: unused_local_variable
-    var darkSTextColor = Colors.black87;
-    var lightTextColor = Colors.grey[100];
-
     final providers = <SingleChildWidget>[
       ChangeNotifierProvider(create: (context) => AuthProvider()),
       ChangeNotifierProvider(create: (context) => StateProvider()),
       ChangeNotifierProvider(create: (context) => DataProvider()),
+      ChangeNotifierProvider(create: (context) => ThemeProvider()),
     ];
-
-    final appBarTheme = AppBarTheme(
-        iconTheme: IconThemeData(color: accentColor),
-        color: darkSurfaceColor,
-        textTheme: TextTheme(
-            headline6: TextStyle(
-          color: lightTextColor,
-        )));
-    final darkTheme = ThemeData(
-      primarySwatch: primary,
-      primaryColor: primaryColor,
-      accentColor: accentColor,
-      appBarTheme: appBarTheme,
-      iconTheme: IconThemeData(
-        color: Colors.purple,
-      ),
-      backgroundColor: darkSurfaceColor,
-      textTheme: TextTheme(
-        bodyText1: TextStyle(color: lightTextColor),
-      ),
-    );
 
     return MultiProvider(
       providers: providers,
       builder: (context, _) =>
-          Consumer3<AuthProvider, StateProvider, DataProvider>(builder:
-              (BuildContext context, AuthProvider auth, StateProvider state,
-                  DataProvider data, _) {
-        ThemeMode _themeMode =
-            state.darkMode ? ThemeMode.dark : ThemeMode.light;
-
+          Consumer4<AuthProvider, StateProvider, DataProvider, ThemeProvider>(
+              builder: (BuildContext context,
+                  AuthProvider auth,
+                  StateProvider state,
+                  DataProvider data,
+                  ThemeProvider theme,
+                  _) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: '@patkellydesigns',
-          themeMode: _themeMode,
-          darkTheme: darkTheme,
-          theme: ThemeData(
-              accentColor: accentColor,
-              primaryColor: primary,
-              textTheme:
-                  TextTheme(bodyText1: TextStyle(color: lightTextColor))),
+          themeMode: theme.themeMode,
+          darkTheme: theme.darkTheme,
+          theme: theme.lightTheme,
           home: Home(),
         );
       }),
---
+++ lib/providers/theme.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  static final primaryColor = Colors.purple;
+  static final accentColor = Colors.yellow[200];
+  static final lightTextColor = Colors.grey[100];
+  static final primary = Colors.purple;
+  static final darkSurfaceColor = Colors.black87;
+  static final lightSurfaceColor = lightTextColor;
+  static final darkTextColor = Colors.black87;
+  ThemeMode themeMode = ThemeMode.system;
+
+  static final lightTextTheme = TextTheme(
+    bodyText1: darkTextStyle,
+    headline1: darkTextStyle,
+    headline2: darkTextStyle,
+    headline3: darkTextStyle,
+    headline4: darkTextStyle,
+    headline5: darkTextStyle,
+    headline6: darkTextStyle,
+    button: darkTextStyle,
+  );
+  static final darkTextTheme = TextTheme(
+    bodyText1: lightTextStyle,
+    headline1: lightTextStyle,
+    headline2: lightTextStyle,
+    headline3: lightTextStyle,
+    headline4: lightTextStyle,
+    headline5: lightTextStyle,
+    headline6: lightTextStyle,
+    button: lightTextStyle,
+  );
+
+  static final lightTextStyle = TextStyle(color: lightTextColor);
+  static final darkTextStyle = TextStyle(color: darkTextColor);
+  static final appBarThemeDark =
+      AppBarTheme(textTheme: darkTextTheme, backgroundColor: darkSurfaceColor);
+  static final appBarThemeLight = AppBarTheme(
+      textTheme: lightTextTheme, backgroundColor: lightSurfaceColor);
+
+  final darkTheme = ThemeData(
+      backgroundColor: darkSurfaceColor,
+      appBarTheme: appBarThemeDark,
+      textTheme: darkTextTheme,
+      primaryColor: primary,
+      accentColor: accentColor,
+      primarySwatch: primaryColor);
+
+  final lightTheme = ThemeData(
+    backgroundColor: lightSurfaceColor,
+    primarySwatch: primaryColor,
+    primaryColor: primary,
+    accentColor: accentColor,
+    textTheme: lightTextTheme,
+    appBarTheme: appBarThemeLight,
+  );
+
+  bool themeDark = false;
+
+  toggleDark() {
+    // Flip the bool
+    themeDark = !themeDark;
+    // Swap modes
+    themeDark ? themeMode = ThemeMode.dark : themeMode = ThemeMode.light;
+    notifyListeners();
+  }
+
+  // Need?
+  ThemeData getTheme() => themeDark ? darkTheme : lightTheme;
+}
